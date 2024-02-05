15e91ea91900b6b414e966d142329326b558f17d
Boka <Maxutca156@yandex.ru>
2024-02-05 18:21:18 +0300

    gg

---
+++ pp.py
@@ -0,0 +1,11 @@
+new*
+class Bill:
+    new*
+    def __int__(self):
+        self.money = 0
+    new*
+    def add(self, count):
+        self.money += count
+    new*
+    def buy(self, count, name ):
+        pass
